576af709ffdd38453dcd8e5da7dfb30981214d50
sy <871772825@qq.com>
2024-03-04 14:16:42 +0800

    feat：water-mark

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello-world-page">
+   <button @click="createWaterMark">生成水印</button>
+  </div>
+</template>
+
+<script>
+import watermark from '@/utils/waterMark'
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+   createWaterMark() {
+     watermark()
+   }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.hello-world-page {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
